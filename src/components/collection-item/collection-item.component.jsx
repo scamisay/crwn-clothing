fd308eb7576aa7dcb23adcbc92a4c9070a9ba4b5
Santiago Camisay <scamisay@candoit.com.ar>
2021-11-24 01:05:36 -0300

    created cart-item component and connected our cart dropdown with cartItem reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './collection-item.styles.scss';
+import { connect } from "react-redux";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
+import CustomButton from "../custom-button/custom-button.component";
 
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
+import { addItem } from "../../redux/cart/cart.actions";
 
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({item, addItem}) => {
+  const { name, imageUrl, price } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to Cart
+      </CustomButton>
     </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
